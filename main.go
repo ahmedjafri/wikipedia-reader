42b65652d53531031b11445fef247004004c0c7b
Ahmed Jafri <me@ahmedjafri.com>
2018-08-14 22:46:55 -0700

    Serialize graph representation out to disk

--- main.go
+++ main.go
@@ -1,65 +1,30 @@
 package main
 
 import (
-	"encoding/xml"
 	"flag"
-	"fmt"
-	"os"
+	"io/ioutil"
+
+	"github.com/inconshreveable/log15"
 )
 
 const (
 	defaultFile = "wikidumps/simplewiki-20170820-pages-meta-current.xml"
-	readAmount  = 1
+	outputFile  = "graph.txt"
+	readAmount  = 1000000
 )
 
 var inputFile = flag.String("infile", defaultFile, "Input XML dump file path")
+var Log = log15.New()
 
 func main() {
 	flag.Parse()
 
-	xmlFile, err := os.Open(*inputFile)
+	g := ReadXMLFile(*inputFile)
+
+	err := ioutil.WriteFile(outputFile, []byte(g.String()), 0644)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer xmlFile.Close()
-
-	decoder := xml.NewDecoder(xmlFile)
-	var total int64
-	var inElement string
-	g := NewGraph()
-
-mainLoop:
-	for {
-		// Read tokens from the XML document in a stream.
-		t, _ := decoder.Token()
-		if t == nil {
-			break
-		}
-		// Inspect the type of the token just read.
-		switch se := t.(type) {
-		case xml.StartElement:
-			// If we just read a StartElement token
-			inElement = se.Name.Local
-			// ...and its name is "page"
-			if inElement == "page" {
-				var p XMLPage
-				// decode a whole chunk of following XML into the
-				// variable p which is a Page
-				decoder.DecodeElement(&p, &se)
-				g.AddXMLPage(p)
-
-				total++
-				if total > readAmount-1 {
-					break mainLoop
-				}
-
-			}
-		default:
-		}
-
+		panic(err)
 	}
 
-	fmt.Printf("Graph:\n%s", g.String())
-	fmt.Printf("Total read articles: %d \n", total)
+	Log.Debug("Total unique articles", "Articles", len(g.NodeMap))
 }
--- xml.go
+++ xml.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/xml"
+	"os"
 	"regexp"
 	"strings"
 )
@@ -16,9 +18,65 @@ type XMLPage struct {
 const linkLimit = 1000
 
 // Limit links to only have two words
-const pageLinkRegexConstraints = "(?P<link>[a-zA-Z0-9]*\\s*[a-zA-Z0-9]*)"
+// The second word cannot contain numbers
+const pageLinkRegexConstraints = "[a-zA-Z0-9]*\\s*[a-zA-Z]*"
 
-var pageLinkRegex = regexp.MustCompile("\\[\\[" + pageLinkRegexConstraints + "\\]\\]")
+var pageLinkRegex = regexp.MustCompile("\\[\\[(?P<link>" + pageLinkRegexConstraints + ")\\]\\]")
+var pageTitleRegex = regexp.MustCompile(pageLinkRegexConstraints)
+
+func ReadXMLFile(filename string) *Graph {
+	xmlFile, err := os.Open(filename)
+	if err != nil {
+		Log.Error("Error opening file", "Error", err)
+		return nil
+	}
+	defer xmlFile.Close()
+
+	decoder := xml.NewDecoder(xmlFile)
+	var total int64
+	var inElement string
+	g := NewGraph()
+
+mainLoop:
+	for {
+		// Read tokens from the XML document in a stream.
+		t, _ := decoder.Token()
+		if t == nil {
+			break
+		}
+		// Inspect the type of the token just read.
+		switch se := t.(type) {
+		case xml.StartElement:
+			// If we just read a StartElement token
+			inElement = se.Name.Local
+			// ...and its name is "page"
+			if inElement == "page" {
+				var p XMLPage
+				// decode a whole chunk of following XML into the
+				// variable p which is a Page
+				decoder.DecodeElement(&p, &se)
+
+				// skip pages whose titles dont match the constraints
+				if !pageTitleRegex.MatchString(p.Title_) {
+					continue
+				}
+
+				g.AddXMLPage(p)
+
+				total++
+				if total > readAmount-1 {
+					break mainLoop
+				}
+
+			}
+		default:
+		}
+
+	}
+
+	Log.Debug("Total read articles", "Articles", total)
+	return g
+}
 
 func (p XMLPage) Title() string {
 	return strings.ToLower(p.Title_)
@@ -27,7 +85,8 @@ func (p XMLPage) Title() string {
 func (p XMLPage) links() []string {
 	r := pageLinkRegex.FindAllStringSubmatch(p.Text, linkLimit)
 	if len(r) >= linkLimit {
-		panic("Find all links matched the limit amount. It could be possible that we have clipped some")
+		Log.Crit("Find all links matched the limit amount. It could be possible that we have clipped some",
+			"Page", p.Title_)
 	}
 
 	rt := make([]string, len(r))
